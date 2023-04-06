24a14951eeec0d6b8916c55c9c7e72ec54ec775e
Hems Bhandari <hemsbhandari04@gmail.com>
2023-04-06 20:07:26 +0545

    Initial Commit

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# minimalist-portfolio
+# Classic Portfolio Site
+![image](https://user-images.githubusercontent.com/60810039/230292696-9eb7423a-af68-48b7-aa25-0ed4843288cf.png)
---
+++ js/script.js
@@ -0,0 +1,90 @@
+$(function(){
+	"use strict";
+
+	/*=========================================================================
+		Initializing stellar.js Plugin
+	=========================================================================*/
+	$('.section').stellar({
+		horizontalScrolling: false
+	});
+	
+	
+	$(window).on('load', function(){
+	
+		$('body').addClass('loaded');
+	
+		
+		/*=========================================================================
+			Portfolio Grid Layout
+		=========================================================================*/
+		var grid = $('#portfolio-grid');
+		grid.shuffle({
+			itemSelector: '.item'
+		});
+		
+		$('#portfolio-filters > ul > li > a').on('click', function (e) {
+			e.preventDefault();
+			var groupName = $(this).attr('data-group');
+			$('#portfolio-filters > ul > li > a').removeClass('active');
+			$(this).addClass('active');
+			grid.shuffle('shuffle', groupName );
+		});
+		
+		$('a.image-link').magnificPopup({
+			type: 'image',
+			removalDelay: 300,
+			mainClass: 'mfp-fade',
+			gallery: {
+				enabled: true
+			}
+		});
+	
+	});
+	
+	
+	
+	/*=========================================================================
+		Links Navigation System
+	=========================================================================*/
+	$('.front-person-links > ul > li > a[data-section]').on('click', function(e){
+		e.preventDefault();
+		var section = $('#' + $(this).data('section'));
+		
+		if( section.size() != 0 ){
+			
+			$('body').addClass('section-show');
+			
+			section.addClass('active');
+		
+		}
+		
+	});
+	$('.close-btn').on('click', function(){
+		$('body').removeClass('section-show');
+		$('section.active').removeClass('active');
+	});
+	
+	
+	
+	/*=========================================================================
+		Testimonials Slider
+	=========================================================================*/
+	$('.testimonials-slider').owlCarousel({
+		singleItem: true
+	});
+	
+	
+	
+	/*=========================================================================
+		Skill Bar's Percent Initialization from attribute data-percent
+	=========================================================================*/
+	$('.skill-bar').each(function(){
+		var $this = $(this),
+			percent = parseInt( $this.data('percent'), 10 );
+		
+		$this.find('.bar').css('width', percent + '%');
+	});
+	
+	
+	
+});
